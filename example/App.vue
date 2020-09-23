d7213b04dc5237b5b23416a8a5d9dc81a1452066
helder.passos <helder.passos@interfy.io>
2020-09-23 17:38:20 -0300

    init

---
+++ example/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="app">
+    <example-initial-dates />
+    <br />
+    <example-language />
+    <br />
+    <example-events />
+    <br />
+    <example-disabled-dates />
+    <br />
+    <example-helper-buttons />
+    <br />
+    <example-time-input-config />
+    <br />
+    <example-date-format />
+    <br />
+    <example-initial-dates />
+    <br />
+    <example-inline />
+  </div>
+</template>
+
+<script>
+import ExampleEvents from './ExampleEvents.vue';
+import ExampleDisabledDates from './ExampleDisabledDates.vue';
+import ExampleHelperButtons from './ExampleHelperButtons.vue';
+import ExampleTimeInputConfig from './ExampleTimeInputConfig.vue';
+import ExampleDateFormat from './ExampleDateFormat.vue';
+import ExampleInitialDates from './ExampleInitialDates.vue';
+import ExampleLanguage from './ExampleLanguage.vue';
+import ExampleInline from './ExampleInline.vue';
+
+export default {
+  components: {
+    ExampleEvents,
+    ExampleDisabledDates,
+    ExampleHelperButtons,
+    ExampleTimeInputConfig,
+    ExampleDateFormat,
+    ExampleInitialDates,
+    ExampleLanguage,
+    ExampleInline,
+  },
+};
+</script>
+
+<style lang="scss">
+.app {
+  padding: 40px;
+}
+
+h2 {
+  margin-left: -20px;
+}
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const path = require('path');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    'vdprDatePicker': './src/Components/DatePicker.vue',
+    'vdprDatePicker.min': './src/Components/DatePicker.vue'
+  },
+  devtool: "source-map",
+  output: {
+    library: 'vdprDatePicker',
+    libraryTarget: 'umd', 
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        include: /\.min\.js$/,
+      }),
+    ],
+  },
+  externals: {
+    moment: 'moment'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          'postcss-loader',
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          'postcss-loader',
+          'sass-loader'
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+  ],
+};
